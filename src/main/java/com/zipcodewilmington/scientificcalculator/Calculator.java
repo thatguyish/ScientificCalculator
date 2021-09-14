0392362f34f49434aad6759449ddd9d2c626c79d
thatguyish <ishmell.ramirez@gmail.com>
2021-09-14 19:59:57 -0400

    start add subtraction

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -1,2 +1,27 @@
-package com.zipcodewilmington.scientificcalculator;public class Calculator {
+package com.zipcodewilmington.scientificcalculator;
+
+public class Calculator {
+
+    private static String onDisplay = "0";
+
+    private static int memory = 0;
+
+    public boolean isActive = true;
+
+    public static void updateCalculator(Double val){
+        onDisplay = String.valueOf(val);
+    }
+
+    public static void calculate(String operator,double val){
+        switch (operator){
+            case "addition":
+                Double sum = val + Double.parseDouble(onDisplay);
+                updateCalculator(sum);
+                break;
+        }
+    }
+
+    public static void showDisplay(){
+        System.out.println(onDisplay);
+    }
 }
--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,17 +1,36 @@
 package com.zipcodewilmington.scientificcalculator;
 
+import java.util.Scanner;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class MainApplication {
     public static void main(String[] args) {
-        Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+        Calculator calculator = new Calculator();
+        Console.println("Welcome to my calculator!");
+        while(calculator.isActive){
+            calculator.showDisplay();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter an Operation");
+            String userInput = scanner.nextLine();
+
+            switch (userInput){
+                case "add":
+                    Double numberToAdd = Console.getDoubleInput("Enter a number to add");
+                    calculator.calculate("addition",numberToAdd);
+
+                    break;
+            }
+//            String s = Console.getStringInput("Enter a string");
+//            Integer i = Console.getIntegerInput("Enter an integer");
+//            Double d = Console.getDoubleInput("Enter a double.");
+//
+//            Console.println("The user input %s as a string", s);
+//            Console.println("The user input %s as a integer", i);
+//            Console.println("The user input %s as a d", d);
+        }
+
     }
 }
